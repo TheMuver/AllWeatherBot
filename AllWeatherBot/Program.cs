e6f1c47feaadfe31399f2b866823118f2d9c7c9f
Muver <via.zyn@ya.ru>
2021-06-12 19:49:48 +0300

    first commit

---
+++ AllWeatherBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllWeatherBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
