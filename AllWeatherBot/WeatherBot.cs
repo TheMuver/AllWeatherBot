c281b02952bbf5b09c566e4703aca9bfe6909026
Muver <via.zyn@ya.ru>
2021-06-16 11:22:17 +0300

    add configureKeyBoard method

--- AllWeatherBot/WeatherBot.cs
+++ AllWeatherBot/WeatherBot.cs
@@ -51,51 +51,56 @@ namespace AllWeatherBot
             Console.WriteLine("Команда: " + command);
 
             string answer;
-            ReplyKeyboardMarkup keyboardMarkup = null;
-            ReplyKeyboardRemove keyboardRemove = new ReplyKeyboardRemove();
             switch (command)
             {
                 case "start":
-                    answer = "Привет! Я бот, дающий тебе информацию о погоде!";
-                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: keyboardRemove);
+                    answer = "Привет! Я бот, дающий тебе информацию о погоде! Для продолжения работы можете ознакомится со списком команд /help";
+                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: new ReplyKeyboardRemove());
                     break;
                 case "help":
                     answer = "Список доступных команд: \n 1. /weather - выводит информацию о погоде";
-                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: keyboardRemove);
+                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: configureKeyboard(new string[] { "/weather" }));
                     break;
                 case "ping":
                     answer = "pong";
-                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: keyboardRemove);
+                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: new ReplyKeyboardRemove());
                     break;
                 case "weather":
-                    //answer = await ConfigureWeatherAnswerAsync();
-                    answer = "12";
-                    keyboardMarkup = new ReplyKeyboardMarkup();
-                    var list = new List<KeyboardButton>();
-                    list.Add(new KeyboardButton("Test"));
-                    keyboardMarkup.Keyboard = new List<IEnumerable<KeyboardButton>>();
-                    keyboardMarkup.Keyboard = keyboardMarkup.Keyboard.Append(list);
-                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: keyboardMarkup);
+                    answer = await ConfigureWeatherAnswerAsync();
+                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: new ReplyKeyboardRemove());
                     break;
                 default:
                     answer = "Неизвестная команда";
-                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: keyboardRemove);
+                    await _client.SendTextMessageAsync(message.Chat.Id, answer, replyMarkup: new ReplyKeyboardRemove());
                     break;
             }
+        }
+
+        private ReplyKeyboardMarkup configureKeyboard(string[] buttons)
+        {
+            var keyboardMarkup = new ReplyKeyboardMarkup();
+            keyboardMarkup.ResizeKeyboard = true;
+            // rows
+            keyboardMarkup.Keyboard = new List<IEnumerable<KeyboardButton>>();
+            // strings
+            keyboardMarkup.Keyboard = keyboardMarkup.Keyboard.Append(buttons.Select(s => new KeyboardButton(s)).ToList());
+            return keyboardMarkup;
         }
 
         private async Task<string> ConfigureWeatherAnswerAsync()
         {
             var weather = await _repository.GetWeatherAsync();
-            var answer =    $"Информация о погоде ({weather.NowDt.Add(new TimeSpan(3, 0, 0))}): \n" +
-                            $" Погода сегодня: \n" + 
+            var answer = $"Информация о погоде ({weather.NowDt.Add(new TimeSpan(3, 0, 0))}): \n" +
+                            $" Погода сегодня: \n" +
                             $"  {weather.Fact.Temp}°C (ощущается как {weather.Fact.FeelsLike}°C) \n" +
-                            $" Погода завтра: \n" + 
-                            $"  {weather.Forecasts[1].Parts.Day.TempAvg}°C";
+                            $" Погода завтра: \n" +
+                            $"  {weather.Forecasts[1].Parts.Day.TempAvg}°C" + 
+                            $"Яндекс.Погода";
             return answer;
         }
 
-        private void foo(int x, int y = 4) {
+        private void foo(int x, int y = 4)
+        {
 
         }
     }
